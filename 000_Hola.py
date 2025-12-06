83b799e7e8e53007b9e7725f92aec76d4e10d379
Palau003 <178561@upslp.edu.mx>
2025-12-06 14:01:27 -0600

    Add files via upload

---
+++ 000_Hola.py
@@ -0,0 +1,12 @@
+# Hola buenas
+""" Krakatoa
+Camaron
+Caramelo
+"""
+print ("Hola wenas")
+
+#Imprimiendo los tipos de datos
+print(type("Krakatoa")) #String
+print(type(False)) #Boolean
+print(type(12.5)) #Float
+print(type(12j)) # Complex
